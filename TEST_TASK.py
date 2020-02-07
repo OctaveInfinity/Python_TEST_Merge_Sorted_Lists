a6926a87740c00e744173cde184246507fbc5289
OctaveInfinity <e.makoviak@gmail.com>
2020-02-07 11:52:41 +0200

    Add comments

--- TEST_TASK.py
+++ TEST_TASK.py
@@ -1,6 +1,8 @@
+# task case
 a = [-1, 8, 10]
 b = [-5, -3, 0, 1, 4, 7, 8, 14]
 
+# additional case
 d = list(range(-20,30,3))
 c = list(range(1,25,2))
 
@@ -8,12 +10,14 @@ c = list(range(1,25,2))
 
 def merge_sorted_lists(a_list, b_list):
 
+    # initialization
     result_list = []
     lenth_a = len(a_list)
     lenth_b = len(b_list)
     a_index = 0
     b_index = 0
 
+    # merge until a shorter list is over
     while a_index < lenth_a and b_index < lenth_b:
         if a_list[a_index] <= b_list[b_index]:
             result_list.append(a_list[a_index])
@@ -22,8 +26,11 @@ def merge_sorted_lists(a_list, b_list):
             result_list.append(b_list[b_index])
             b_index += 1
 
+    # add last part of a longer list
     result_list.extend(a_list[a_index:] + b_list[b_index:])
     return result_list
 
+# execution task case
 print(merge_sorted_lists(a, b))
+# execution additional case
 print(merge_sorted_lists(d, c))
